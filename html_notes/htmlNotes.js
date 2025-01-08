f00df9285a449e802e6941fdc5bc0884a4255d64
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2025-01-07 18:35:49 -0600

    html

---
+++ html_notes/htmlNotes.js
@@ -0,0 +1,34 @@
+//!    ¿Que es html?
+
+//todo   Hyper Text markup language   --->   El ESQUELETO
+// * HYPER TEXT Lenguaje que contiene enlaces (HIPERVINCULOS) a otros documentos o recursos.
+// *Markup Funciona mediante etiquetas
+
+// Lenguaje estandar para la creacion de paginas web.
+// Describe la estructura 
+// Indica al explorador que y como mostrarlo.
+
+//!    ¿COMO FUNCIONA?
+
+// Estructura de arbol 
+// Inicia con un nodo padre, y de ahi se desgloza hacia abajo.
+
+
+//!    ¿COMO FUNCIONA UNA ETIQUETA?
+
+// Etiqueta funcion por tres partes:
+// Apertura / Mensaje / Cierre
+
+// ! Secciones basicas 
+
+// Html 
+    // head
+    // body
+        // etiquetas
+
+
+
+//! por convencion se llama al archivo con extencion html = index.html 
+
+
+    //todo  :  ! + tecla tab  === Generar la plantilla de html5 basica.
