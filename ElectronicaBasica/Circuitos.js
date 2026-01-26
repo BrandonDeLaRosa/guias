e81f7b19f1b39e458036acb797d0551efaafc839
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2026-01-26 17:31:17 -0600

    electronica basica

---
+++ ElectronicaBasica/Circuitos.js
@@ -0,0 +1,52 @@
+// !Circuitos.
+
+    // Conjunto de elementos conectados entre si.
+
+//! Circuito impreso (PCB).
+    // Placa donde se interactuan componentes erlectronicos 
+
+//! Circuito cerrado.   Conectado  
+// ! Circuito abierto.     Desconectado
+
+
+//! Corto circuito en fuente (Voltaje). 
+
+    //todo Cuando en un circuito el polo positivo se conecta a un negativo, esto eleva la corriente de forma brusca y el voltaje baja a cero. 
+
+
+//! Corto circuito en componente electronico.
+    // Cuando en un dispositivo electronico se conectan sus polos negativo a positivo o uniendo sus pines.
+  
+
+//! Corriente alterna y correinte directa.
+
+    //todo LA correinte alterna es la corriente de hogar, se le llama de esa forma porque la energia va fluctuando entre ciclo positivo y negativo generando una onda senoidal.
+        //? Se usa en casas porque se puede transformar facilmente, se puede transportar sin perder corriente.
+        //? Cambia de positivo a negativo
+        //? Red electrica
+        //? Contactos
+        //? en mexico se mide 127V AC 60Hz (cambia 60 veces por segundo.)
+    
+    
+    //todo La corriente directa, se usa mayormente en dispositivos electronicos, tiene un voltaje contante , no cambia de direccion 
+
+        //? Los electrones se mueven siempre en el mismo sentido.
+        //? Voltaje constante.
+        //? Baterias, arduino, USB Fuentes de 12 a 24 v.
+
+//! Diferencias entre Ac / Dc
+
+    // La corriente alterna atraviesa mejor la piel y puede afectar organos vitales, se acompla mejor a tejidos , nervios y musculos.  //! VCA / VAC
+    // LA ccorreitne alterna 127 60Hz al entrar en contacto provoca contraccion uscular evitando que la victima pueda soltarse.     //! VCC / VDC
+    // Dependiendo la potencia es que tantos organos puede afectar.
+
+
+
+
+     
+
+
+
+
+
+
---
+++ ElectronicaBasica/Conceptos-basicos.js
@@ -0,0 +1,69 @@
+// Conceptos basicos de electronica. 4 conceptos basicos.
+
+
+    // --Conductores
+
+        // ? Conductor: Material que permite el paso de corriente electrica.
+        // ? Cable: Objeto fisico heco de material conductor(cobre) +  material aislante (plastico).
+        // ? Material conductor: CObre, aluminio,etc
+        // ? Material aislante: plastico , vidrio, madera, 
+
+
+   // Donde viajan los electrones?  
+        // ? Los electrones ya estan en el material conductor, solo se desplazan cuando se aplica voltaje.
+    
+   //! Corriente
+
+        // ? Movimiento de electrones a traves de un conductor (electrones se mueven === corriente || electrones no se mueven === no hay corriente).
+        // * Cantidad de carga electrica fluyendo porun conductor en un tiempo determinado (Se mide en amperes "A").
+        // todo puede haber voltaje sin corriente, pero nunca corriente sin voltaje.
+
+    // Elementos para que exista corriente:
+            // Conductor.
+            // Fuente o voltaje.
+            // Circuito cerrado. 
+
+
+    //! Voltaje        
+
+        //? No es la fuerza con la que se desplazan los electrones. Es la precion electrica que provoca el movimiento de electrones.
+        // todo El voltaje es DIFERENCIA DE POTENCIAL.
+        // bateria crea diferencia de potencial (voltaje)
+        // se genera campo electrico dentro del conductor.
+        // El campo empuja los electrones al ejecer fuerza sobre ellos.
+        // Los electrones se mueven y se genera una corriente.
+
+        // todo se mide en VOlT.
+
+
+        // Voltaje alto no significa corriente alta
+
+        // La corriente depende también de la resistencia
+
+    // Ejemplo:
+
+        // Batería 9V + circuito abierto → 0 corriente
+
+        // Batería 1.5V + corto → corriente enorme
+    
+    
+    // ! Potencia 
+    
+        //todo Cantidad de energia consume/entrega por segundo o tiempo determinado.  Se mide en WATTS --> "W"
+        //todo Potencia === voltaje x Corriente
+        //todo La potencia es el efecto final. (Calor, luz,movimiento,energia).
+
+
+    // ! Resistencia:
+
+        // ? Es la oposicion al flujo de los electrones o al paso de la corriente.
+        // ? Frena el flujo de la corriente.
+
+
+
+    //! Funcionamiento en conjunto:
+    
+        // Voltaje: Empuja electrones ---> Volts --> V
+        // Resistencia: Frena electrones  ---> Ohms --->  simbolo omega o la palabra ohm
+        // Corriente: Mo vimiento de electrones --> Amperios --> A o I
+        // Potencia: Resultado (Luz, calor, movimiento, energia.)  Watts  --> W o V (De vatio). pero comunmente siempre es w para no confundir con volt
