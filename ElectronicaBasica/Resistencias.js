4d8eda9099fa3ba765db6bae6f14bf59ef7f7fc2
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2026-01-26 23:55:50 -0600

    resitencias.

---
+++ ElectronicaBasica/Resistencias.js
@@ -0,0 +1,21 @@
+//! Resitencias.
+
+
+    // Se van a leer de lado donde tienen mas lineas agrupadas a donde solo tiene 1 
+
+    // ? Las primeras das indican un nuemero (Significativas).
+    // ? La tercer linea es la multiplicadora (Cauntos ceros se aumentan ).
+    // ? la ultima del lado contrario, se llama Tolerancia.
+
+
+    4700
+
+    // 22000 5%  11+/-
+
+    // 510   Tolerancia 0.2    0.5 kilo  ohms con +/- .2 de tolerancia.
+
+
+    // 270   5%  T = 0.13
+
+
+    //kOhms 4,700  %%  2.3
