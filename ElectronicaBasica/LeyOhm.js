989a4a08aa433addbdcd3705bc2ed07881685097
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2026-01-28 00:06:35 -0600

    ley de ohm

---
+++ ElectronicaBasica/LeyOhm.js
@@ -0,0 +1,76 @@
+//! Circuitos resisitivos
+
+
+// ! Ley de ohm (Sin ella nada en electronica puede existir).
+
+// todo I = intensidad / A amperio == Ambos sgnifican corriente.
+
+
+// V = I * R   (Voltaje es igual a corriente x resistencia).
+// ? Un voltio es igual a un amperio x un ohm.
+
+
+// I = V / R
+// ? Corriente es igual a voltaje entre resistencia.
+
+
+// R = V / I
+// ? Resistencia es igual a voltaje entre corriente/amperios/intensidad
+
+
+
+//  ! Tipos de fuentes.
+
+// ? Fuentes dependientes y fuentes independientes 
+
+
+
+// ! Ejemplos:
+
+    // * Si la resistencia aumtenta disminuye la corriente y visceversa.
+
+//! Circuitos serie.
+
+    // Componente tras de otro.
+
+    // Todo la corriente electrica siemopre circula de positivo a negativo.
+
+    // POLARIZACION = a partir de la fuente, la corriente va viajar
+    // Del polo positivo de fuente a polo positivo de la  terminal positiva de la resitencia (aunque no tengan polos + o -)
+    // Es solo para saber el flujo (Fuente  +) --> (+ Resitencia - ) --> ( + Resistencia -) --->  ( + Resistencia -) ---> ( - Fuente  +)   (Ahi completa el circuito).
+//                                                                  (Nodo)                  (nodo)
+    // ? Nodo (punto de inteseccion de elementos en el ejemplo de arriba los nodos son los puntos de contacto)
+
+    
+                                                             //todo(VF)     // todo cod. de color: Cafe, negro, naranja, Dorado = 10000 / 1000 = 10k 
+    // En este ejemplo se considera un circuito serie: Voltaje Fuente(15V), 3 Resitencias de 10k
+
+        // Todo : En este ejemplo la I === en cualquier parte del circuito.
+        // TOdo : El V de la fuente se divide por cada compoente = c/Resistencia === 5V. 
+        //TODO  : Resitencia total es = suma de las resitencias. (En este ejemplo 30k).
+
+
+
+    //? (Se toma primero I porque ya se conoce el valor de la fuente = V y de las resistencias === 30.000 ohms) Necesitamos sacar los amperios del circuito. El circuito consume 0.5mA de consumo.
+    
+    // se divide el voltaje entre la resitencia === 15v / 30,000 ohms ===  0.0005 AMP / 5mA  (mili amperios).  
+
+    //  Para sacar el V usamos la ley de ohm  v = I * R    0.0005 * 10000 = 5v. (Se usan los valores de c/r por separado).
+
+    //  Resitencia R = V / I   5 / 0.0005
+
+
+    // todo   mayor resitencia mayor voltaje / menor resitencia menor voltaje
+    // todo menor resitencia mayor corriente / mayor resitencia menor corriente.  
+
+
+
+
+
+
+
+
+
+
+
+
