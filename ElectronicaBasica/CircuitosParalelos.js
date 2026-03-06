1a86b3769f1ebe10fe9863c56ccff2a30defc904
Brandon De La Rosa <110071390+BrandonDeLaRosa@users.noreply.github.com>
2026-03-06 15:23:25 -0600

    up

---
+++ ElectronicaBasica/CircuitosParalelos.js
@@ -0,0 +1,10 @@
+// ! Circuitos paralelos.  
+
+    //  En una fuente la corriente va de - A  +     En un circuito la corriente va de + a -
+    // todo en estos circuitos la corriente se divide entre los componentes, i1, i2, i3 ...
+    // todo el voltaje eel mismo en v1, v2 ,v3 ...
+
+
+    // todo la suma de las resitencias, para sacar la resitencia total es del final del circuito hacia la fuente, no al reves.
+    // * La RT = 1 /  1/r1 + 1/r2 + 1/r3 ...
+    // * Para sumar de 2 en 2 resitencias: RT = R1 + R2 / R1 + R2
